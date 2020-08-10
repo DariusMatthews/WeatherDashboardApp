cd13ad3881508c8a48af5ff1fcb801384757b7a5
Darius Matthews <DariustheDeveloper@gmail.com>
2020-08-10 14:00:34 -0500

    Set up local storage for city names.

--- app.js
+++ app.js
@@ -3,20 +3,31 @@ $(document).ready(function () {
   // DOM Variables
   const cityName = $('.city-name');
   const searchBtn = $('.city-search');
+  const weatherSection = $('.weather-forecast');
 
   // City List Array
-  let cityList = [];
+  let cityList = !localStorage.getItem('city list') ? [] : JSON.parse(localStorage.getItem('city list'));
 
   // Search Functionality
   searchBtn.on('click', function () {
-    // API URL
+
+    // Map City name to Weather API URL
     const weatherURL = `http://api.openweathermap.org/data/2.5/weather?q=${cityName.val()}&appid=bb9ce599fcfd1ddd0bc51db84a830cfd&units=imperial`;
 
     // Getting Weather Data
     getWeather(weatherURL);
 
     // Add city to city list;
+    cityList.push(cityName.val());
+
+    // Add city list to local storage
+    localStorage.setItem('city list', JSON.stringify(cityList));
+
+    // Display city list
   });
+
+  // Add City Buttons to list after each search
+
 
   const getWeather = url => {
     // API CALL CURRENT WEATHER
@@ -24,17 +35,19 @@ $(document).ready(function () {
       url: url,
       method: 'GET'
     }).then(function (response) {
-      const weatherSection = $('.weather-forecast');
       console.log(response)
+
       // store coordinates
       let coord = {
         lat: response.coord.lat,
         lon: response.coord.lon
       }
 
+      // map coords to forecast API URL
       const forecastURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&
       exclude=current,minutely,hourly&appid=bb9ce599fcfd1ddd0bc51db84a830cfd`
 
+      // get 5 day forecast
       getForecast(forecastURL);
     });
   }
